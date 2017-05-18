f2c36a111b6185af849dc90d87b175e72c5f2807
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2017-05-18 17:49:11 -0500

    add minimap position data attrs

--- partials/map.php
+++ partials/map.php
@@ -58,35 +58,35 @@
 
 <div id="minimap">
   <div class="minimap-row grid-row">
-    <div class="minimap-button">
+    <div class="minimap-button" data-pos='{"row": 0,"col": 0}'>
       <span>&bull;</span>
     </div>
-    <div class="minimap-button">
+    <div class="minimap-button" data-pos='{"row": 0,"col": 1}'>
       <span>&bull;</span>
     </div>
-    <div class="minimap-button">
-      <span>&bull;</span>
-    </div>
-  </div>
-  <div class="minimap-row grid-row">
-    <div class="minimap-button">
-      <span>&bull;</span>
-    </div>
-    <div class="minimap-button">
-      <span>&bull;</span>
-    </div>
-    <div class="minimap-button">
+    <div class="minimap-button" data-pos='{"row": 0,"col": 2}'>
       <span>&bull;</span>
     </div>
   </div>
   <div class="minimap-row grid-row">
-    <div class="minimap-button">
+    <div class="minimap-button" data-pos='{"row": 1,"col": 0}'>
       <span>&bull;</span>
     </div>
-    <div class="minimap-button">
+    <div class="minimap-button" data-pos='{"row": 1,"col": 1}'>
       <span>&bull;</span>
     </div>
-    <div class="minimap-button">
+    <div class="minimap-button" data-pos='{"row": 1,"col": 2}'>
+      <span>&bull;</span>
+    </div>
+  </div>
+  <div class="minimap-row grid-row">
+    <div class="minimap-button" data-pos='{"row": 2,"col": 0}'>
+      <span>&bull;</span>
+    </div>
+    <div class="minimap-button" data-pos='{"row": 2,"col": 1}'>
+      <span>&bull;</span>
+    </div>
+    <div class="minimap-button" data-pos='{"row": 2,"col": 2}'>
       <span>&bull;</span>
     </div>
   </div>
