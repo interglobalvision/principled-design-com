34dd790449456cb51696c34f98b93501bda84b93
cas <cas@interglobal.vision>
2017-05-23 15:48:58 +0900

    add request ip info from ipdb and return to client in a js var

--- lib/functions-custom.php
+++ lib/functions-custom.php
@@ -1,3 +1,34 @@
 <?php
+require "dbip-client.class.php";
 
 // Custom functions (like special queries, etc)
+
+function get_client_geolocation() {
+  $client_ip = get_client_ip();
+
+  $api_key = IGV_get_option('_igv_site_options', '_igv_dbip_api_key');
+
+  $dbip = new DBIP_Client($api_key);
+
+  return $dbip->Get_Address_Info($client_ip);
+
+}
+
+// Return a string of the client ip
+function get_client_ip() {
+  if (!empty( $_SERVER['HTTP_CLIENT_IP'])) { //check ip from share internet
+
+    $ip = $_SERVER['HTTP_CLIENT_IP'];
+
+  } elseif ( ! empty( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) { //to check ip is pass from proxy
+
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+
+  } else {
+
+    $ip = $_SERVER['REMOTE_ADDR'];
+
+  }
+
+  return apply_filters( 'wpb_get_ip', $ip );
+}
