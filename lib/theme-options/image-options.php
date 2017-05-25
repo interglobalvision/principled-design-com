e88ba118c6f403976212e30ac95689d8f6e597e2
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2017-05-25 09:42:47 -0500

    image options to repeatable group

--- lib/theme-options/image-options.php
+++ lib/theme-options/image-options.php
@@ -6,51 +6,54 @@ $suffix = '_options';
 $page_key = $prefix . 'image' . $suffix;
 $page_title = 'Image Options';
 
-$fields = array();
-
-for ($i = 1; $i < 10; $i++) {
-  $fields[] = array(
-    'name' => __( 'Map Grid ' . $i, 'cmb2' ),
-    'desc' => __( '', 'cmb2' ),
-    'id'   => $prefix . $i . '_title',
-    'type' => 'title',
-  );
-
-  $fields[] = array(
+$fields = array(
+  array(
     'name' => __( 'Image', 'IGV' ),
     'desc' => __( '', 'cmb2' ),
-    'id'   => $prefix . $i . '_image',
+    'id'   => 'image',
     'type' => 'file',
-  );
-
-  $fields[] = array(
+  ),
+  array(
     'name'    => __( 'MP4 Video', 'IGV' ),
     'desc'    => __( 'if displaying as a video (optional)', 'IGV' ),
-    'id'      => $prefix . $i . '_mp4',
+    'id'      => 'mp4',
     'type'    => 'file',
-  );
-
-  $fields[] = array(
+  ),
+  array(
     'name'    => __( 'WEBM Video', 'IGV' ),
     'desc'    => __( 'if displaying as a video (optional)', 'IGV' ),
-    'id'      => $prefix . $i . '_webm',
+    'id'      => 'webm',
     'type'    => 'file',
-  );
-
-  $fields[] = array(
+  ),
+  array(
     'name'    => __( 'OGV Video', 'IGV' ),
     'desc'    => __( 'if displaying as a video (optional)', 'IGV' ),
-    'id'      => $prefix . $i . '_ogv',
+    'id'      => 'ogv',
     'type'    => 'file',
-  );
-}
+  ),
+);
 
 $metabox = array(
   'id'         => $page_key, //id used as tab page slug, must be unique
   'title'      => $page_title,
   'show_on'    => array( 'key' => 'options-page', 'value' => array( $page_key ), ), //value must be same as id
   'show_names' => true,
-  'fields'     => $fields
+  'fields'     => array(
+    array(
+      'name' => __( 'Map Grid Group', 'cmb2' ),
+      'desc' => __( '', 'cmb2' ),
+      'id'   => $prefix . 'map_grid_group',
+      'type' => 'group',
+      'repeatable'  => true,
+      'options'     => array(
+        'group_title'   => __( 'Item {#}', 'cmb2' ),
+        'add_button'    => __( 'Add Another Item', 'cmb2' ),
+        'remove_button' => __( 'Remove Item', 'cmb2' ),
+        'sortable'      => true,
+      ),
+      'fields' => $fields,
+    ),
+  ),
 );
 
 IGV_init_options_page($page_key, $page_key, $page_title, $metabox);
