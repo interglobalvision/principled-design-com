f875d28129bebdabb9e9fccbf303ffdce30c4601
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2017-05-25 10:24:14 -0500

    randomize grid items

--- partials/map.php
+++ partials/map.php
@@ -10,37 +10,68 @@
   </div>
 
 <?php
-  for ($i = 1; $i < 10; $i++) {
-    $image_id = IGV_get_option('_igv_image_options', '_igv_' . $i . '_image_id');
-    $mp4 = IGV_get_option('_igv_image_options', '_igv_' . $i . '_mp4');
-    $webm = IGV_get_option('_igv_image_options', '_igv_' . $i . '_webm');
-    $ogv = IGV_get_option('_igv_image_options', '_igv_' . $i . '_ovg');
+  global $grid_group, $group_length, $used_items;
+  $grid_group = IGV_get_option('_igv_image_options','_igv_map_grid_group');
+  $group_length = count($grid_group);
+  $used_items = array();
+
+  function make_map_grid($item) {
+    global $grid_group, $used_items;
+
+    // push $item (group index) into used items array
+    array_push($used_items, $item);
+
+    // shorten the variable name
+    $item = $grid_group[$item];
 ?>
-  <div class="map-block">
-    <div class="map-block-content">
-      <?php
-        if (empty($mp4)) {
-          echo wp_get_attachment_image($image_id, 'map');
-        } else if ($mp4 && $webm) {
-          $poster = wp_get_attachment_image_src($image_id, 'map');
-      ?>
-      <video autoplay muted poster="<?php echo $poster[0]; ?>">
-        <source src="<?php echo $mp4; ?>" type="video/mp4" />
-        <source src="<?php echo $webm; ?>" type="video/webm" />
-      <?php
-        if ($ogv) {
-      ?>
-        <source src="<?php echo $ogv; ?>" type="video/ogv" />
-      <?php
-        }
-      ?>
-      </video>
-      <?php
-        }
-      ?>
-    </div>
+<div class="map-block">
+  <div class="map-block-content">
+    <?php
+      if (empty($item['mp4'])) {
+        // just show the image
+        echo wp_get_attachment_image($item['image_id'], 'map');
+      } else if (!empty($item['mp4']) && !empty($item['webm'])) {
+        // show the video
+        $poster = wp_get_attachment_image_src($item['image_id'], 'map');
+    ?>
+    <video autoplay muted poster="<?php echo $poster[0]; ?>">
+      <source src="<?php echo $item['mp4']; ?>" type="video/mp4" />
+      <source src="<?php echo $item['webm']; ?>" type="video/webm" />
+    <?php
+      if (!empty($item['ovg'])) {
+    ?>
+      <source src="<?php echo $item['ovg']; ?>" type="video/ogv" />
+    <?php
+      }
+    ?>
+    </video>
+    <?php
+      }
+    ?>
   </div>
+</div>
 <?php
+  }
+
+  function call_map_grid() {
+    global $grid_group, $group_length, $used_items;
+
+    // get a random index from grid group
+    $item = mt_rand(0, $group_length - 1);
+
+    if (!in_array($item, $used_items) && !empty($grid_group[$item]['image_id'])) {
+      // item index is unused, and has image
+      // make the grid item
+      make_map_grid($item);
+    } else {
+      // get a new item index and try again
+      call_map_grid();
+    }
+  }
+
+  for ($i = 0; $i < $group_length; $i++) {
+    // for each item in group, make grid
+    call_map_grid();
   }
 ?>
 </div>
