31b9fc9ec3d46fdf5e3d508ce8741d7804f8a530
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2017-05-25 11:39:45 -0500

    fix empty conditionals

--- lib/functions-custom.php
+++ lib/functions-custom.php
@@ -7,25 +7,27 @@ function make_map_grid_unit($item) {
 <div class="map-block">
   <div class="map-block-content">
     <?php
-      if (empty($item['mp4'])) {
-        // just show the image
-        echo wp_get_attachment_image($item['image_id'], 'map');
-      } else if (!empty($item['mp4']) && !empty($item['webm'])) {
-        // show the video
-        $poster = wp_get_attachment_image_src($item['image_id'], 'map');
+      if (!empty($item['image_id']) {
+        if (!empty($item['mp4']) && !empty($item['webm'])) {
+          // show the video
+          $poster = wp_get_attachment_image_src($item['image_id'], 'map');
     ?>
     <video autoplay muted poster="<?php echo $poster[0]; ?>">
       <source src="<?php echo $item['mp4']; ?>" type="video/mp4" />
       <source src="<?php echo $item['webm']; ?>" type="video/webm" />
     <?php
-      if (!empty($item['ovg'])) {
+          if (!empty($item['ovg'])) {
     ?>
       <source src="<?php echo $item['ovg']; ?>" type="video/ogv" />
     <?php
-      }
+          }
     ?>
     </video>
     <?php
+        } else {
+          // just show the image
+          echo wp_get_attachment_image($item['image_id'], 'map');
+        }
       }
     ?>
   </div>
